6ed0b38cdffeb6cdf71811b1fdb5e9c979f2c769
DaeBin HA <dbha@sk.com>
2021-12-20 14:10:13 +0900

    update

--- src/main/java/com/dbha/jenkins/myapptest/HelloController.java
+++ src/main/java/com/dbha/jenkins/myapptest/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Spring Boot Push test";
+        return "Spring Boot Push test1";
     }
 }
