1ddb9f8ca17384ba5a65a1e548821866c13f7b00
DaeBin HA <dbha0719@gmail.com>
2021-12-24 11:52:11 +0900

    Change Hello print

--- src/main/java/com/dbha/jenkins/myapptest/HelloController.java
+++ src/main/java/com/dbha/jenkins/myapptest/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Spring Boot New Image Test!!";
+        return "Spring Boot New Image TEST2";
     }
 }
