544c7d890704570b9a43cc4552ffecb43e3a7ec0
dbha <dbha0719@gmail.com>
2022-01-26 18:55:42 +0900

    update

--- src/main/java/com/dbha/jenkins/myapptest/HelloController.java
+++ src/main/java/com/dbha/jenkins/myapptest/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Spring Boot Test Dependency";
+        return "Spring Boot Test Dependency TEST";
     }
 }
